da0d062d60b039964d6312ac3986407c6510e153
Misha20112000 <misha20112000web@gmail.com>
2020-09-16 09:42:19 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//other import
+import React from 'react';
+//styles
+//my components
+
+export const App = () => {
+    return (
+        <div>
+            hello app
+        </div>
+    )
+};
+
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+//other import
+import React from 'react';
+import ReactDOM from 'react-dom';
+//styles
+//my components
+import {App} from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
