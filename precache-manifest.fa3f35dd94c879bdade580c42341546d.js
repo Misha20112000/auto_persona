8bed3cc62a39adf998b8db6b5f9222d9a178dda8
Misha20112000 <misha20112000web@gmail.com>
2020-09-30 13:55:31 +0300

    Updates

---
+++ precache-manifest.fa3f35dd94c879bdade580c42341546d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b2b7d0869e26f3fb8e6bd625446f0b",
+    "url": "/auto_persona/index.html"
+  },
+  {
+    "revision": "f692831ae68b63a56864",
+    "url": "/auto_persona/static/css/main.0d46e876.chunk.css"
+  },
+  {
+    "revision": "1fb9853f0379ae75d928",
+    "url": "/auto_persona/static/js/2.6c09c56f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/auto_persona/static/js/2.6c09c56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f692831ae68b63a56864",
+    "url": "/auto_persona/static/js/main.47a70f7f.chunk.js"
+  },
+  {
+    "revision": "da96d48c466bf2bfef05",
+    "url": "/auto_persona/static/js/runtime-main.8ae20f3b.js"
+  },
+  {
+    "revision": "8a1e8457a3482d98f2501dceeae0890b",
+    "url": "/auto_persona/static/media/bg.8a1e8457.jpg"
+  },
+  {
+    "revision": "77c8fb5234c7cb26223eb15464b372bb",
+    "url": "/auto_persona/static/media/bg_1.77c8fb52.jpg"
+  },
+  {
+    "revision": "bb8abfdf1b65dbe807c888a824b2f99d",
+    "url": "/auto_persona/static/media/bg_2.bb8abfdf.jpg"
+  },
+  {
+    "revision": "c241681bf331286cc825653f6f6c3420",
+    "url": "/auto_persona/static/media/bg_3.c241681b.jpg"
+  },
+  {
+    "revision": "0ee360acc18db685c7030cefe2aa57f6",
+    "url": "/auto_persona/static/media/bg_4.0ee360ac.jpg"
+  },
+  {
+    "revision": "39abd7806fd0c548e9d86218458c3534",
+    "url": "/auto_persona/static/media/cart.39abd780.svg"
+  },
+  {
+    "revision": "d96ac15e486562c69592005deba2928d",
+    "url": "/auto_persona/static/media/copyright.d96ac15e.svg"
+  },
+  {
+    "revision": "43d8b2d4de62a33dd6b6532dc52a4d7d",
+    "url": "/auto_persona/static/media/courier.43d8b2d4.jpg"
+  },
+  {
+    "revision": "9acd9db119b1c90d157441022d83c1f6",
+    "url": "/auto_persona/static/media/dagger.9acd9db1.svg"
+  },
+  {
+    "revision": "9062ede666008789f1481a8d5f88cf8c",
+    "url": "/auto_persona/static/media/emptyStar.9062ede6.svg"
+  },
+  {
+    "revision": "b9416a2fbcf42d30c4a62582f4798f49",
+    "url": "/auto_persona/static/media/facebook.b9416a2f.svg"
+  },
+  {
+    "revision": "024ffb8da592f090f087d68ed224f4f7",
+    "url": "/auto_persona/static/media/goldSkull.024ffb8d.jpg"
+  },
+  {
+    "revision": "66f3026d41519ca1770875fb7cc60085",
+    "url": "/auto_persona/static/media/instagram.66f3026d.svg"
+  },
+  {
+    "revision": "12053df8c7822c14b90393940bfdca51",
+    "url": "/auto_persona/static/media/newPost.12053df8.jpg"
+  },
+  {
+    "revision": "6cceafafe72a9f13b7793db95ec174a9",
+    "url": "/auto_persona/static/media/selfPickup.6cceafaf.jpg"
+  },
+  {
+    "revision": "44d7b1b97546f338a4101e935b9ffb1d",
+    "url": "/auto_persona/static/media/star.44d7b1b9.svg"
+  },
+  {
+    "revision": "8dcb752062296a0f30fa0a4dae828945",
+    "url": "/auto_persona/static/media/telegram.8dcb7520.svg"
+  },
+  {
+    "revision": "38acc09a23377c19b8b0d5cf723e203a",
+    "url": "/auto_persona/static/media/ukrPost.38acc09a.jpg"
+  },
+  {
+    "revision": "5e24fa6696e6a70dc532c3626953e433",
+    "url": "/auto_persona/static/media/viber.5e24fa66.svg"
+  },
+  {
+    "revision": "d868ced194af3a1a9bcc4a20779ce781",
+    "url": "/auto_persona/static/media/whatsApp.d868ced1.svg"
+  }
+]);
